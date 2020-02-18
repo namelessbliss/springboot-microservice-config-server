836891811c904c5b6c4f40916e00bb384b924e87
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-17 19:23:37 -0500

    Proyecto Inicial

---
+++ src/main/java/com/nb/springbootmicroserviceconfigserver/SpringbootMicroserviceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroserviceConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nb/springbootmicroserviceconfigserver/SpringbootMicroserviceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
